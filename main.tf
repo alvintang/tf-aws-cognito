eb208f67ed41338c3bfe8f050b130e04f5d5db18
Alvin Tang <alvintang@gmail.com>
2021-04-21 11:43:59 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = var.cognito_user_pool_name
+}
+
+resource "aws_cognito_user_pool_domain" "domain" {
+  domain       = var.cognito_domain_name
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = var.cognito_client_name
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  callback_urls = [ "http://localhost:8000/callback" ]
+  logout_urls = [ "https://localhost:8000/logout" ]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows = ["code"]
+  allowed_oauth_scopes = [ "email", "openid", "profile" ]
+  supported_identity_providers = [ "Google", "COGNITO" ]
+}
+
+resource "aws_cognito_identity_provider" "example_provider" {
+  user_pool_id  = aws_cognito_user_pool.pool.id
+  provider_name = "Google"
+  provider_type = "Google"
+
+  provider_details = {
+    authorize_scopes = "profile email openid"
+    client_id        = sensitive(var.google_client_id)
+    client_secret    = sensitive(var.google_client_secret)
+  }
+
+  attribute_mapping = {
+    email    = "email"
+    username = "sub"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "cognito_user_pool_name" {
+    type = string
+}
+variable "cognito_domain_name" {
+    type = string
+}
+variable "cognito_client_name" {
+    type = string
+}
+variable "google_client_id" {
+    type = string
+}
+variable "google_client_secret" {
+    type = string
+}
